c75d4074a4a0fb7414cfc9a61ac1e3a40e59cfe3
Luis Pedraza <luispedraza@gmail.com>
2016-02-11 18:12:56 +0100

    finished simple GeoQuiz app with one single question

--- GeoQuiz/app/src/main/java/es/luispedraza/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/es/luispedraza/geoquiz/QuizActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -25,14 +26,14 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
             }
         });
 
