72c6bdae96cc21fdd9bb6b2c984120c78d8b33f6
Luis Pedraza <luispedraza@gmail.com>
2016-02-14 19:15:44 +0100

    showing cheat information

--- GeoQuiz/app/src/main/java/es/luispedraza/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/es/luispedraza/geoquiz/CheatActivity.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "es.luispedraza.geoquiz.answer_is_true";
     boolean mAnswerisTrue;
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswerButton;
 
     public static Intent createNewIntent(Context context, boolean answerIsTrue) {
         Intent i = new Intent(context, CheatActivity.class);
@@ -22,5 +28,15 @@ public class CheatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cheat);
 
         mAnswerisTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView) findViewById(R.id.cheat_text_view);
+
+        mShowAnswerButton = (Button) findViewById(R.id.show_cheat_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAnswerTextView.setText(mAnswerisTrue ? R.string.true_button : R.string.false_button);
+            }
+        });
     }
 }
