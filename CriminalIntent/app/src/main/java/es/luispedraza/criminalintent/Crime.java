25210451797d9af96bc8153f4f14b0cc1adb296a
Luis Pedraza <luispedraza@gmail.com>
2016-02-16 22:10:34 +0100

    added a new class Crime as model

---
+++ CriminalIntent/app/src/main/java/es/luispedraza/criminalintent/Crime.java
@@ -0,0 +1,29 @@
+package es.luispedraza.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by luis on 16/2/16.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime(String title) {
+        mTitle = title;
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
