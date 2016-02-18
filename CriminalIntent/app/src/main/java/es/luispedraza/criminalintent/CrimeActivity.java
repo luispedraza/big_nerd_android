cbfba1831cdda24643a90d9388672d9fd2a54316
Luis Pedraza <luispedraza@gmail.com>
2016-02-18 11:33:28 +0100

    finished the fragment activity

--- CriminalIntent/app/src/main/java/es/luispedraza/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/es/luispedraza/criminalintent/CrimeActivity.java
@@ -1,7 +1,9 @@
 package es.luispedraza.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -9,5 +11,17 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        // Now, add the CrimeFragment:
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
+
     }
 }
